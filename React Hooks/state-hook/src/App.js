41dea87ff35fbe141fe4c46e120afc2d44af1e0a
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-03-30 01:11:16 +0530

    class counter

--- React Hooks/state-hook/src/App.js
+++ React Hooks/state-hook/src/App.js
@@ -1,15 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ClassCounter from "./ClassCounter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <ClassCounter />
     </div>
   );
 }
