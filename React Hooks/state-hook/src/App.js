4d8e94ea02471369fa59050b7699229160a4f914
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-03-30 01:21:21 +0530

    Hook counter

--- React Hooks/state-hook/src/App.js
+++ React Hooks/state-hook/src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ClassCounter from "./ClassCounter";
+import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
 
 function App() {
   return (
     <div className="App">
       <ClassCounter />
+      <HookCounter />
     </div>
   );
 }
